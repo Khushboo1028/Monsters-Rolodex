2ae0286df932080247618a4c1a2113d30b33275c
Khushboo <khushboo1028@gmail.com>
2020-06-17 00:32:52 +0530

    Updates

---
+++ precache-manifest.d0f7bb20f8bc365459a15518d6900e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0043f0cc96f4f587775f46d66bf901b4",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "f7fbc0ed59c22a4baa6b",
+    "url": "/Monsters-Rolodex/static/css/main.79906f41.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7fbc0ed59c22a4baa6b",
+    "url": "/Monsters-Rolodex/static/js/main.692468e1.chunk.js"
+  },
+  {
+    "revision": "bb1db29ffdd7a4ded20c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f852ebba.js"
+  }
+]);
